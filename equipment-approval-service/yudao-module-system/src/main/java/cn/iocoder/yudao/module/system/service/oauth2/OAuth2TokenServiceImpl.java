ddd0489fcc41d63160d072d84ca23bebd083aa32
Irreplaceable <1349694984@qq.com>
2025-09-12 14:55:44 +0800

    oauth2 登录用户info修改 证书列表增加状态

--- equipment-approval-service/yudao-module-system/src/main/java/cn/iocoder/yudao/module/system/service/oauth2/OAuth2TokenServiceImpl.java
+++ equipment-approval-service/yudao-module-system/src/main/java/cn/iocoder/yudao/module/system/service/oauth2/OAuth2TokenServiceImpl.java
@@ -206,8 +206,9 @@ public class OAuth2TokenServiceImpl implements OAuth2TokenService {
             return MapUtil.builder(LoginUser.INFO_KEY_NICKNAME, user.getNickname())
                     .put(LoginUser.INFO_KEY_DEPT_ID, StrUtil.toStringOrNull(user.getDeptId())).build();
         } else if (userType.equals(UserTypeEnum.MEMBER.getValue())) {
-            // 注意：目前 Member 暂时不读取，可以按需实现
-            return Collections.emptyMap();
+            AdminUserDO user = adminUserService.getUser(userId);
+            return MapUtil.builder(LoginUser.INFO_KEY_NICKNAME, user.getNickname())
+                    .put(LoginUser.INFO_KEY_DEPT_ID, StrUtil.toStringOrNull(user.getDeptId())).build();
         }
         return null;
     }
